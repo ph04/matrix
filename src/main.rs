4b52a2748a422dc414abed4353b65b29178691d9
ph04 <47356461+ph04@users.noreply.github.com>
2021-03-26 19:50:41 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,20 @@ use matrix::matrix::Matrix;
 fn main() {
     let m1 = Matrix::new([
         [1.0, 4.0, 6.0],
-    ]).unwrap();
+    ]);
 
-    let m2 = Matrix::new([
+    let mut m2 = Matrix::new([
         [2.0, 3.0],
         [5.0, 8.0],
         [7.0, 9.0]
-    ]).unwrap();
+    ]);
 
     let m3 = Matrix::new([
         [1.0, 2.0],
         [3.0, 4.0],
-    ]).unwrap();
+    ]);
 
-    println!("{}", m3 * m3);
+    m2.swap_row(0, 2);
+
+    println!("{}", m2);
 }
